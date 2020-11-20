d91862b478bedacff15086d5f04a1353fcc0a6c1
Thais RIbeiro <thaisribeiro.mesquita@gmail.com>
2020-11-20 17:59:50 -0300

    Criado model e controller de usuário

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import routes from './routes';
 
 class App {
     constructor() {
         this.server = express();
+        mongoose.connect('mongodb+srv://cinema-app:cinema-app@cinema-app.my7xy.mongodb.net/cinema-app?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
         this.middlewares();
         this.routes();
     }
